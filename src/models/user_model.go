c64073ba7f2bcec3a3bb5c7b505dabb615175bb4
Vasilije <vasilijezzzz@gmail.com>
2025-07-24 16:34:58 +0200

    feat: Improve `User` model

--- src/models/user_model.go
+++ src/models/user_model.go
@@ -1,12 +1,28 @@
 package model
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
+
+type UserRole string
+
+const (
+	Guest UserRole = "guest"
+	Host  UserRole = "host"
+	Admin UserRole = "admin"
+)
+
 type User struct {
-	gorm.Model
-	ID       uint   `gorm:"primaryKey"`
-	Username string `json:"username" gorm:"unique;not null;type:string"`
-	Password string `json:"password" gorm:"not null;type:string"`
-	Email    string `json:"email" gorm:"unique;not null;type:string"`
+	ID        uint     `                gorm:"primaryKey"`
+	AddressID uint     `                gorm:"not null;"`
+	Username  string   `json:"username" gorm:"type:varchar(30);not null;uniqueIndex"`
+	Password  string   `json:"password" gorm:"type:varchar(100);not null;"`
+	Email     string   `json:"email"    gorm:"type:varchar(60);not null;uniqueIndex"`
+	Name      string   `json:"name"     gorm:"type:varchar(60);not null;"`
+	Surname   string   `json:"surname"  gorm:"type:varchar(60);not null;"`
+	Role      UserRole `json:"role"     gorm:"type:varchar(5);not null;check:role IN ('guest','host','admin')"`
+	Address   Address  `json:"address"`
+}
 }
