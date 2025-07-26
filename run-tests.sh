23866cec58505184680617c072e6d92e99266aa2
Vasilije <vasilijezzzz@gmail.com>
2025-07-26 12:21:01 +0200

    feat: Cover all packages under the `src` directory

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo "Running tests..."
 
 cd src
-go test -coverprofile=coverage.out ./...
+go test -coverprofile=coverage.out -coverpkg=./... ./...
 go tool cover -func=coverage.out
 
 echo "Linting..."
