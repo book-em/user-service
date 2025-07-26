dfb9fb2ff7baedbfce5aeb6083bd0864719eeab3
Vasilije <vasilijezzzz@gmail.com>
2025-07-26 12:19:31 +0200

    feat: Improve validation by leveraging `gin`
    - I also tried to improve error handling.
    - Typo: `ErrUserExists` → `ErrUsernameExists`

--- src/domain/address.go
+++ src/domain/address.go
@@ -2,9 +2,9 @@ package domain
 
 type Address struct {
 	ID      uint   `                gorm:"primaryKey"`
-	Street  string `json:"street"   gorm:"type:varchar(80);not null"`
-	City    string `json:"city"     gorm:"type:varchar(60);not null"`
+	Street  string `json:"street"   gorm:"type:varchar(80);not null" binding:"required"`
+	City    string `json:"city"     gorm:"type:varchar(60);not null" binding:"required"`
 	State   string `json:"state"    gorm:"type:varchar(60)"`
 	Zip     string `json:"zip"      gorm:"type:varchar(20)"`
-	Country string `json:"country"  gorm:"type:varchar(80);not null"`
+	Country string `json:"country"  gorm:"type:varchar(80);not null" binding:"required"`
 }
