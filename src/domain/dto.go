93958255fcb4012d2a3562f85be00e2e4c056cd7
Vasilije <vasilijezzzz@gmail.com>
2025-07-26 16:02:42 +0200

    fix: Remove `address` model, use `string` instead

--- src/domain/dto.go
+++ src/domain/dto.go
@@ -1,11 +1,11 @@
 package domain
 
 type UserDTO struct {
-	Username string  `json:"username" binding:"required,max=30"`
-	Password string  `json:"password" binding:"required,min=4,max=30"`
-	Email    string  `json:"email"    binding:"required,max=60,email"`
-	Name     string  `json:"name"     binding:"required,max=60"`
-	Surname  string  `json:"surname"  binding:"required,max=60"`
-	Role     string  `json:"role"     binding:"required,oneof=guest host admin"`
-	Address  Address `json:"address"  binding:"required"`
+	Username string `json:"username" binding:"required,max=30"`
+	Password string `json:"password" binding:"required,min=4,max=30"`
+	Email    string `json:"email"    binding:"required,max=60,email"`
+	Name     string `json:"name"     binding:"required,max=60"`
+	Surname  string `json:"surname"  binding:"required,max=60"`
+	Role     string `json:"role"     binding:"required,oneof=guest host admin"`
+	Address  string `json:"address"  binding:"required,max=150"`
 }
--- src/domain/user.go
+++ src/domain/user.go
@@ -9,13 +9,12 @@ const (
 )
 
 type User struct {
-	ID        uint     `                gorm:"primaryKey"`
-	AddressID uint     `                gorm:"not null;"`
-	Username  string   `json:"username" gorm:"type:varchar(30);not null;uniqueIndex"`
-	Password  string   `json:"password" gorm:"type:varchar(100);not null;"`
-	Email     string   `json:"email"    gorm:"type:varchar(60);not null;uniqueIndex"`
-	Name      string   `json:"name"     gorm:"type:varchar(60);not null;"`
-	Surname   string   `json:"surname"  gorm:"type:varchar(60);not null;"`
-	Role      UserRole `json:"role"     gorm:"type:varchar(5);not null;check:role IN ('guest','host','admin')"`
-	Address   Address  `json:"address"`
+	ID       uint     `                gorm:"primaryKey"`
+	Username string   `json:"username" gorm:"type:varchar(30);not null;uniqueIndex"`
+	Password string   `json:"password" gorm:"type:varchar(100);not null;"`
+	Email    string   `json:"email"    gorm:"type:varchar(60);not null;uniqueIndex"`
+	Name     string   `json:"name"     gorm:"type:varchar(60);not null;"`
+	Surname  string   `json:"surname"  gorm:"type:varchar(60);not null;"`
+	Role     UserRole `json:"role"     gorm:"type:varchar(5);not null;check:role IN ('guest','host','admin')"`
+	Address  string   `json:"address"  gorm:"type:varchar(150);not null"`
 }
