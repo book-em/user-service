ea77b0a7a3ff52b0fd0b77deeb1c518c425e2c8d
magley <60552482+magley@users.noreply.github.com>
2025-08-01 12:24:05 +0200

    feat: Add integration tests to workflow

--- run-tests.sh
+++ run-tests.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# This is before pipefail in case the dir already exists.
+mkdir test-out
+
 set -euo pipefail
-
-echo "Running tests..."
-
 cd src
-go test -coverprofile=coverage.out -coverpkg=./... ./...
-go tool cover -func=coverage.out
+
+echo "Running unit tests..."
+
+go test -v -coverprofile=./test-out/coverage-unit.out -coverpkg=./... ./test/unit/...
+
+echo "Building coverage report..."
+
+go tool cover -func=./test-out/coverage-unit.out
+go tool cover -html=./test-out/coverage-unit.out -o ./test-out/coverage-unit.html
 
 echo "Linting..."
 
 go vet ./...
-
-echo "All checks passed!"
