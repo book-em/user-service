467528c4f8d013e3ee41c00796a441b931420f0f
magley <60552482+magley@users.noreply.github.com>
2025-08-06 17:34:35 +0200

    test: Write more unit tests for updating user

--- src/test/unit/update_test.go
+++ src/test/unit/update_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"testing"
 
 	domain "bookem-user-service/domain"
@@ -38,4 +39,102 @@ func TestUpdate_Success(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, userAfter, *newUser)
+}
+
+func TestUpdate_SomeoneElse(t *testing.T) {
+	mockRepo := new(MockRepo)
+	svc := service.NewService(mockRepo)
+
+	// Prepare
+
+	newName := "new123"
+
+	dto := domain.UserUpdateDTO{Id: 123, Username: &newName}
+
+	// Verify
+
+	newUser, err := svc.Update(1, dto)
+
+	assert.Nil(t, newUser)
+	assert.Error(t, err)
+	assert.Equal(t, domain.ErrUnauthorized, err)
+}
+
+func TestUpdate_UserNotFound(t *testing.T) {
+	mockRepo := new(MockRepo)
+	svc := service.NewService(mockRepo)
+
+	// Prepare
+
+	newName := "new123"
+
+	dto := domain.UserUpdateDTO{Id: 1, Username: &newName}
+
+	// Mock
+
+	mockRepo.On("FindById", uint(1)).Return(nil, fmt.Errorf("user not found"))
+
+	// Verify
+
+	newUser, err := svc.Update(uint(1), dto)
+
+	assert.Nil(t, newUser)
+	assert.Error(t, err)
+	//assert.Equal(t, domain.ErrUnauthorized, err)
+}
+
+func TestUpdate_UsernameTaken(t *testing.T) {
+	mockRepo := new(MockRepo)
+	svc := service.NewService(mockRepo)
+
+	// Prepare
+
+	oldName := "user123"
+	newName := "new123"
+
+	userBefore := domain.User{ID: 1, Username: oldName}
+	dto := domain.UserUpdateDTO{Id: 1, Username: &newName}
+
+	exitingUser := domain.User{ID: 123}
+
+	// Mock
+
+	mockRepo.On("FindById", uint(1)).Return(&userBefore, nil)
+	mockRepo.On("FindByUsernameOrEmail", newName, "").Return(&exitingUser)
+
+	// Verify
+
+	newUser, err := svc.Update(1, dto)
+
+	assert.Nil(t, newUser)
+	assert.Error(t, err)
+	assert.Equal(t, domain.ErrUsernameExists, err)
+}
+
+func TestUpdate_EmailTaken(t *testing.T) {
+	mockRepo := new(MockRepo)
+	svc := service.NewService(mockRepo)
+
+	// Prepare
+
+	oldName := "user123@email.com"
+	newName := "new123@email.com"
+
+	userBefore := domain.User{ID: 1, Email: oldName}
+	dto := domain.UserUpdateDTO{Id: 1, Email: &newName}
+
+	exitingUser := domain.User{ID: 123}
+
+	// Mock
+
+	mockRepo.On("FindById", uint(1)).Return(&userBefore, nil)
+	mockRepo.On("FindByUsernameOrEmail", "", newName).Return(&exitingUser)
+
+	// Verify
+
+	newUser, err := svc.Update(1, dto)
+
+	assert.Nil(t, newUser)
+	assert.Error(t, err)
+	assert.Equal(t, domain.ErrEmailExists, err)
 }
