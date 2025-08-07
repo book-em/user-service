1bc661a583430ce99f3602d3591058af5ebcfa65
magley <60552482+magley@users.noreply.github.com>
2025-08-07 17:45:09 +0200

    test: Write tests for FindById

---
+++ src/test/unit/find_test.go
@@ -0,0 +1,44 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	service "bookem-user-service/service"
+
+	assert "github.com/stretchr/testify/assert"
+)
+
+func TestFindById_Success(t *testing.T) {
+	mockRepo := new(MockRepo)
+	svc := service.NewService(mockRepo)
+
+	id := uint(1)
+
+	user := defaultUser
+	user.ID = id
+
+	mockRepo.On("FindById", id).Return(user, nil)
+	userGot, err := svc.FindById(id)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, userGot)
+	assert.Equal(t, user.ID, userGot.ID)
+
+}
+
+func TestFindById_UserNotFound(t *testing.T) {
+	mockRepo := new(MockRepo)
+	svc := service.NewService(mockRepo)
+
+	id := uint(1)
+
+	user := defaultUser
+	user.ID = id
+
+	mockRepo.On("FindById", id).Return(nil, fmt.Errorf("no such user"))
+	userGot, err := svc.FindById(id)
+
+	assert.Error(t, err)
+	assert.Nil(t, userGot)
+}
