be11efa3623f2b8f3f428d8d3a0c9734defc5c4d
magley <60552482+magley@users.noreply.github.com>
2025-10-09 14:35:16 +0200

    feat: Create Prometheus middleware

---
+++ src/api/middleware/prometheus_middleware.go
@@ -0,0 +1,43 @@
+package middleware
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "status", "endpoint"},
+	)
+
+	httpResponseSizeBytes = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_response_size_bytes",
+			Help: "Total response size in bytes",
+		},
+		[]string{"endpoint", "status"},
+	)
+)
+
+func PrometheusMiddleware() gin.HandlerFunc {
+	prometheus.MustRegister(httpRequestsTotal)
+	prometheus.MustRegister(httpResponseSizeBytes)
+
+	return func(c *gin.Context) {
+		c.Next()
+
+		endpoint := c.FullPath()
+		status := fmt.Sprintf("%d", c.Writer.Status())
+		method := c.Request.Method
+		size := float64(c.Writer.Size())
+
+		httpRequestsTotal.WithLabelValues(method, status, endpoint).Inc()
+		httpResponseSizeBytes.WithLabelValues(endpoint, status).Add(size)
+	}
+}
