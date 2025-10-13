f171d2e01f6d8af17203eabb1e0d7819a1383a6d
Vasilije <vasilijezzzz@gmail.com>
2025-10-13 13:14:02 +0200

    feat: Move logic of `GetActiveHostReservations` to `reservation-service`

--- src/client/reservationclient/client.go
+++ src/client/reservationclient/client.go
@@ -13,6 +13,9 @@ type ReservationClient interface {
 	// GetActiveGuestReservations finds all reservations made by `guest` that
 	// haven't completed yet. The user must be a guest.
 	GetActiveGuestReservations(ctx context.Context, jwt string) ([]ReservationDTO, error)
+	// GetActiveHostReservations finds all reservations made to rooms owned by
+	// `host` that haven't completed yet. The user must be a host.
+	GetActiveHostReservations(ctx context.Context, jwt string) ([]ReservationDTO, error)
 }
 
 type reservationClient struct {
@@ -51,6 +54,39 @@ func (c *reservationClient) GetActiveGuestReservations(ctx context.Context, jwt 
 	var obj []ReservationDTO
 	if err := json.Unmarshal(bodyBytes, &obj); err != nil {
 		utils.TEL.Error("JSON unmarshall error", err)
+		return nil, err
+	}
+
+	return obj, nil
+}
+
+func (c *reservationClient) GetActiveHostReservations(ctx context.Context, jwt string) ([]ReservationDTO, error) {
+	utils.TEL.Push(ctx, "get-active-reservations-for-host")
+	defer utils.TEL.Pop()
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/reservations/host/active", c.baseURL), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", "Bearer "+jwt)
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		utils.TEL.Error("error ", err)
+		return nil, err
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		utils.TEL.Error("parsing response error", err)
+
+		return nil, err
+	}
+
+	var obj []ReservationDTO
+	if err := json.Unmarshal(bodyBytes, &obj); err != nil {
+		utils.TEL.Error("JSON unmarshall error", err)
+
 		return nil, err
 	}
 
--- src/client/roomclient/model.go
+++ src/client/roomclient/model.go
@@ -1,7 +1,5 @@
 package roomclient
 
-import "time"
-
 type RoomDTO struct {
 	ID          uint     `json:"id"`
 	HostID      uint     `json:"hostID"`
@@ -12,19 +10,6 @@ type RoomDTO struct {
 	MaxGuests   uint     `json:"maxGuests"`
 	Photos      []string `json:"photos"`
 	Commodities []string `json:"commodities"`
-}
-
-type ReservationDTO struct {
-	ID                 uint      `gorm:"primaryKey"`
-	RoomID             uint      `gorm:"not null"`
-	RoomAvailabilityID uint      `gorm:"not null"`
-	RoomPriceID        uint      `gorm:"not null"`
-	GuestID            uint      `gorm:"not null"`
-	DateFrom           time.Time `gorm:"not null"`
-	DateTo             time.Time `gorm:"not null"`
-	GuestCount         uint      `gorm:"not null"`
-	Cancelled          bool      `gorm:"not null"`
-	Cost               uint      `gorm:"not null"`
 }
 
 type CreateRoomDTO struct {
